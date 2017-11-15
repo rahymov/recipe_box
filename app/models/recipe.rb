3341b7c6c3639db93553a600d04e33215283e988
Yazgylych <y.rahymov@gmail.com>
2017-11-15 08:21:08 -0500

    Added devise and style to sign in sign out pages

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,10 @@
 class Recipe < ApplicationRecord
 	
+	belongs_to :user
+	
 	has_many :ingredients
 	has_many :directions
+
 	accepts_nested_attributes_for :ingredients,
 																reject_if: proc {|attributes| attributes['name'].blank?},
 																allow_destroy: true
